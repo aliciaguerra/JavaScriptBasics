f5016a2ed268df5b111163a6a9b82c73c1d8a6f1
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-30 20:44:36 -0700

    Create switchmonth.js

---
+++ switchmonth.js
@@ -0,0 +1,22 @@
+switch(monthId){
+case 0:
+case 1:
+case 2:
+ displayQuarter="Q1";
+ break;
+case 3:
+case 4:
+case 5:
+ displayQuarter="Q2";
+ break;
+case 6;
+case 7;
+case 8;
+  displayQuarter="Q3";
+  break;
+case 9;
+case 10;
+case 11;
+ displayQuarter;
+ break;
+ }
