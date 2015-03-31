fdf784f3a15e94a424a7c132409a72f54de46269
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-30 20:26:21 -0700

    Create switchmatchvalues.js

---
+++ switchmatchvalues.js
@@ -0,0 +1,21 @@
+switch(month){
+case 0:
+displayMonth="January"
+break
+case 1:
+displayMonth="February"
+break
+case 2:
+displayMonth="March"
+break
+case 3:
+displayMonth="April"
+break
+case 4:
+displayMonth="May"
+break
+case 5:
+displayMonth="June"
+break
+default displayMonth="invalid
+}
