dfd28b56278e30b5fdad78d104d6de0de7059261
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-30 19:58:00 -0700

    Create ifelseexample.js

---
+++ ifelseexample.js
@@ -0,0 +1,11 @@
+if(navigator.appName=='Microsoft Internet Explorer"){
+document.write("You're running Microsoft IE")
+}
+else{
+if(navigator.appName=="NetScape"){
+document.write("You're running NetScape")
+}
+else{
+document.write("You're not running Microsoft IE or NetScape.")
+}
+}
