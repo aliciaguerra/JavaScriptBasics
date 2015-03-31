8cb2363bb99cbe5199120e650ea6823e8f57419c
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-31 11:30:11 -0700

    Create whileloop.js

---
+++ whileloop.js
@@ -0,0 +1,10 @@
+var totalInventory=700
+var NumberPurchased=200
+var numberSales=0
+
+while(totalInventory>numberPurchase){
+totalInventory=totalInventory-numberPurchased
+numberSales++
+}
+document.write.ln("Our stock supply will support" +
+numberSales + "one of those book sales")
